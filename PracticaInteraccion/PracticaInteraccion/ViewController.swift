6263f13abb116a5183fbf64f5c0dff8f037385ae
Alberth-h <jesalbertohyfx@hotmail.com>
2021-08-26 17:46:28 -0700

    PracticaInteraccion

---
+++ PracticaInteraccion/PracticaInteraccion/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PracticaInteraccion
+//
+//  Created by Alumno on 8/26/21.
+//  Copyright © 2021 ULSA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController { 
+
+    @IBOutlet weak var lblNombre: UILabel!
+    @IBOutlet weak var lblResultado: UILabel!
+    @IBOutlet weak var txtEdad: UITextField!
+    
+    @IBAction func doTapBtnDecirEdad(_ sender: Any) {
+        lblResultado.text = "La persona \(lblNombre.text!) tiene \(txtEdad.text!) años"
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        lblNombre.text = "Alberto Hoyos"
+        // Do any additional setup after loading the view.
+    }
+    
+}
+
---
+++ PracticaUI/PracticaUI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PracticaUI
+//
+//  Created by Alumno on 8/24/21.
+//  Copyright © 2021 ULSA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
