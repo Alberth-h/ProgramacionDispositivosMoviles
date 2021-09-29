36ae2b195192894487851bf1658fa4a8426c170d
Alberto Hoyos <53828141+Alberth-h@users.noreply.github.com>
2021-09-28 17:48:47 -0700

    PracticaLista

---
+++ PracticaLista/PracticaLista/CeldaAlumnoController.swift
@@ -0,0 +1,16 @@
+//
+//  CeldaAlumnoController.swift
+//  PracticaLista
+//
+//  Created by Alexia Ruiz Quiroz on 28/09/21.
+//
+
+import Foundation
+import UIKit
+
+class CeldaAlmunoController: UITableViewCell {
+    @IBOutlet weak var lblNombre: UILabel!
+    @IBOutlet weak var lblMatricula: UILabel!
+    @IBOutlet weak var lblPromedio: UILabel!
+    
+}
---
+++ PracticaLista/PracticaLista/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  PracticaLista
+//
+//  Created by Alexia Ruiz Quiroz on 28/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    let nombres = ["jose", "ana", "maria", "pedro"]
+    let matriculas = ["9999", "4567", "2764", "1739"]
+    let promedios = ["9.9", "8.3", "10.0", "7.6"]
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 81
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return nombres.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let celda = tableView.dequeueReusableCell(withIdentifier: "celdaAlumno")
+        return celda!
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
